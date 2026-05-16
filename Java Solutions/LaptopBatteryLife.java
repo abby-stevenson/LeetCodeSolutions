988267be8f345ba4ab55db073adcf3d3148c55ba
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2026-05-16 16:56:29 -0400

    Laptop Battery Life (Java)

---
+++ Java Solutions/LaptopBatteryLife.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double timeCharged = Double.parseDouble(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+        System.out.printf("%.2f\n", Math.min(2 * timeCharged, 8.0));
+    }
+}
